758267288fde1744e4cbf9b8377d928b23201d4f
AtikhaA <atikhazmila@gmail.com>
2022-07-11 18:47:03 +0700

    js

--- hapi-web-server/server.js
+++ hapi-web-server/server.js
@@ -1,1 +1,23 @@
-console.log('Halo, kita akan belajar membuat server menggunakan Hapi');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+ 
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+ 
+    server.route({
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            const { username, password } = request.payload;
+            return `Welcome ${username}!`;
+        },
+    });
+ 
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
